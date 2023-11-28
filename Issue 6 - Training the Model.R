f51fe1de1b7bc0abbe7d166d100467f251297f83
Njeri Njuguna <joyce.njuguna@starthmore.edu>
2023-11-28 03:40:12 +0300

    Your commit message here

--- Issue 6 - Training the Model.R
+++ Issue 6 - Training the Model.R
@@ -29,15 +29,60 @@ dim(testing_data)
 #of a statistic, such as the mean or variance, and can be applied to assess the stability and 
 #reliability of your model.
 # Install and load the boot package
-install.packages("boot")
-library(boot)
-# Specify the number of bootstrap samples
-num_bootstraps <- 1000
 
-# Set the seed for reproducibility
+# Bootstrap for mean
+set.seed(123)
+boot_means <- replicate(1000, mean(sample(Hoteldata$Price, replace = TRUE)))
+
+# Confidence interval for the mean
+mean_ci <- quantile(boot_means, c(0.025, 0.975))
+
+# Bootstrap for median
+set.seed(123)
+boot_medians <- replicate(1000, median(sample(Hoteldata$Price, replace = TRUE)))
+
+# Confidence interval for the median
+median_ci <- quantile(boot_medians, c(0.025, 0.975))
+
+# Bootstrap for standard deviation
+set.seed(123)
+boot_sds <- replicate(1000, sd(sample(Hoteldata$Price, replace = TRUE)))
+
+# Confidence interval for the standard deviation
+sd_ci <- quantile(boot_sds, c(0.025, 0.975))
+
+
+
+install.packages("boot")
+
+
+
+
+# Load necessary libraries
+library(boot)
+
+# Set seed for reproducibility
 set.seed(123)
 
-# Create a function that performs resampling and fits the model
+# Function to fit your model
+bootstrap_function <- function(data, indices) {
+  # Select data using the indices
+  resampled_data <- data[indices, ]
+  
+  # Fit the model on the resampled data
+  model_formula <- Rating ~ Type + Price + Place + ReviewsCount
+  model <- lm(model_formula, data = resampled_data)
+  
+  # Return the model parameters or performance metric of interest
+  return(coef(model))  # Returning coefficients for simplicity, you can modify this based on your needs
+}
 
+# Set the number of bootstrap samples
+num_bootstraps <- 100
 
+# Perform bootstrapping to create multiple training sets and train models
+bootstrap_results <- boot(data = Hoteldata, statistic = bootstrap_function, R = num_bootstraps)
+
+# Print summary of bootstrapping results
+summary(bootstrap_results)
 
